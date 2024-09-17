3b058c060d73066fac019359b4539c8c230960c0
ESKGH <evgenij.kovalev.1999@mail.ru>
2024-09-17 13:20:27 +0300

    fix final

--- my-react-app/src/components/helper/apihelper.ts
+++ my-react-app/src/components/helper/apihelper.ts
@@ -1,6 +1,5 @@
 const API_KEY = '92ff7663';
 
-
 export const fetchMovies = async (query: string) => {
   try {
     const response = await fetch(`http://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`);
@@ -8,9 +7,9 @@ export const fetchMovies = async (query: string) => {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.json();
-    return data.Search;
+    return data.Search || [];
   } catch (error) {
     console.error('Error fetching movies:', error);
-    return null; 
+    return [];
   }
 };
